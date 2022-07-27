99da87415d033afd316c4326dd3c9aa0b5a15e5f
unknown <kooshasamadi@gmail.com>
2022-07-27 18:32:49 +0430

    responsive navigation added

--- src/components/navigation/navigation.components.jsx
+++ src/components/navigation/navigation.components.jsx
@@ -1,38 +1,42 @@
 import "./navigation.styles.scss";
-import { Button, Menu } from "antd";
 import React, { useState } from "react";
 import { AreaChartOutlined } from "@ant-design/icons";
-import { AntDesignOutlined } from "@ant-design/icons";
 import { Avatar } from "antd";
 import { Link, Outlet } from "react-router-dom";
-
+import Exit from "../../assets/exit.png";
 const Navigation = () => {
+  const [open, setOpen] = useState(true);
   return (
     <>
-      <div className="navigation-container">
+      <div className={`navigation-container ${!open ? "close" : ""}`}>
         <div className="logo">
           <Avatar
-            size={70}
+            size={80}
             icon={<AreaChartOutlined />}
             style={{ backgroundColor: "transparent", color: "white" }}
           />
-          <p>Crypto World</p>
+          <h1>Crypto World</h1>
         </div>
-        <Menu theme="dark">
-          <Menu.Item>
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/exchanges">Exchanges</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/news">News</Link>
-          </Menu.Item>
-        </Menu>
+        <ul className="menu-container">
+          <Link to="/">Home</Link>
+
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+
+          <Link to="/exchanges">Exchanges</Link>
+
+          <Link to="/news">News</Link>
+        </ul>
+        <img src={Exit} className="cross" onClick={() => setOpen(!open)} />
       </div>
+      <div className="navbar-smalldevice">
+        <h1>Crypto World</h1>
+        <img
+          src="https://img.icons8.com/ios-filled/25/000000/menu--v1.png"
+          className="hamburger"
+          onClick={() => setOpen(!open)}
+        />
+      </div>
+
       <Outlet />
     </>
   );
